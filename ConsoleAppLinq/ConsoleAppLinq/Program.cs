c3dec4f4542b4c69d2714def76afdbb3ce732c74
Chester Heng <chester.heng@gmail.com>
2019-11-10 04:05:45 +0800

    01:create a new console app

---
+++ ConsoleAppLinq/ConsoleAppLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
