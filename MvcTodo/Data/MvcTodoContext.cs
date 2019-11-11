5231509e8b9a2ae0a57e76fa774e8385a7291bae
Chester Heng <chester.heng@gmail.com>
2019-11-12 05:40:14 +0800

    04:Create a database context class

---
+++ MvcTodo/Data/MvcTodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcTodo.Models;
+
+namespace MvcTodo.Data
+{
+    public class MvcTodoContext: DbContext
+    {
+        public MvcTodoContext(DbContextOptions<MvcTodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItem { get; set; }
+    }
+}
